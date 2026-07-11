9ce89c51e18697887c3b3362e8dc1e01d8c8e1c5
Pari Bainsla <pbainsla@qualys.com>
2026-07-12 00:02:00 +0530

    Added Hello World Program

---
+++ first_code.py
@@ -0,0 +1,39 @@
+# print("Hello Pari")
+# print no
+# print(90)
+# print("90")
+# print('90')
+
+# declaring varaible
+# name = "Paru";
+# print(name)
+
+# '''comment'''
+
+
+#Sum of two numbers
+# number1 = 10;
+# number2 = 67;
+# sum = number1 + number2;
+# print(sum);
+
+
+# changing values
+# name = "xyz"
+# print(name)
+# name = "abc"
+# print(name)
+
+
+# a=1; b="uio";
+# print(a);
+# print(b);
+
+# a,b=1, "pari";
+# print(a,b);
+# print(b,a);
+
+# a=b="pari";
+# print(a,b);
+
+print("Hello, World!")
