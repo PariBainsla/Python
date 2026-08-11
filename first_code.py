c8c83de2fc75668cc454df3024f1c91dae10fc27
Pari Bainsla <paribainsla161@gmail.com>
2026-08-11 15:24:24 +0530

    identity operators

--- first_code.py
+++ first_code.py
@@ -137,6 +137,15 @@
 # print(a!=b)
 
 
-a=6
-b=9
-print((a>2) and (b<2));
+# a=6
+# b=9
+# print((a>2) and (b<2));
+
+# a=7;
+# b=9;
+# c="pari";
+# d='bainsla';
+# e='pari';
+
+# print(c is not e);
+# print (a is b);
