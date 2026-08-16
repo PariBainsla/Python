3bf240c0e253641f770ff49be67f6a378fdea513
Pari Bainsla <paribainsla161@gmail.com>
2026-08-16 21:03:16 +0530

    new

--- first_code.py
+++ first_code.py
@@ -177,3 +177,6 @@ entered_code = input("Enter passcode: ");
 
 result = (passcode == entered_code);
 print(f"Is passcode == entered code? {result}");
+
+result = (passcode != entered_code);
+print(f"Is passcode not equal to entered code? {result}");
